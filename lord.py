7e1f42e5331cf91dbad38ff1920a176379e54d73
Axel <avi.marsden@mail.huji.ac.il>
2020-07-06 00:16:50 +0300

    added dtype float32 to identity_embedding input layer

--- lord.py
+++ lord.py
@@ -77,8 +77,7 @@ def train(args):
 
 	data = np.load(assets.get_preprocess_file_path(args.data_name))
 	imgs, identities, poses, n_identities = data['imgs'], data['identities'], data['poses'], data['n_identities']
-	print(identities.dtype)
-	identities = identities.astype(np.float32)
+	identities = identities.astype(np.int64)
 	imgs = imgs.astype(np.float32) / 255.0
 
 	converter = Converter.build(
