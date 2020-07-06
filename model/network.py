06bfc4e97e9b6d6e8637f2e5ed17d5ea0662312c
Axel <avi.marsden@mail.huji.ac.il>
2020-07-06 16:30:46 +0300

    casted self.optimizer.weights to float before iterating

--- model/network.py
+++ model/network.py
@@ -13,6 +13,8 @@ from keras.models import Model, load_model
 from keras.callbacks import ReduceLROnPlateau, EarlyStopping, Callback
 from keras.applications import vgg16
 from keras_lr_multiplier import LRMultiplier
+from keras_lr_multiplier.backend import optimizers
+
 
 from model.evaluation import EvaluationCallback, TrainEncodersEvaluationCallback
 
@@ -124,8 +126,8 @@ class Converter:
 		model = Model(inputs=[img, identity], outputs=generated_img)
 
 		model.compile(
-			optimizer=LRMultiplier(
-				optimizer=optimizers.Adam(beta_1=0.5, beta_2=0.999, dtype=np.float32),
+			optimizer=LRMultiplierWrapper(
+				optimizer=optimizers.Adam(beta_1=0.5, beta_2=0.999),
 				multipliers={
 					'identity-embedding': 10.0
 				}
@@ -447,3 +449,85 @@ class CosineLearningRateScheduler(Callback):
 
 		K.set_value(self.model.optimizer.lr, lr)
 		logs['lr'] = lr
+
+
+class LRMultiplierWrapper(optimizers.Optimizer):
+
+	def __init__(self, optimizer, multipliers, **kwargs):
+		super(LRMultiplierWrapper, self).__init__(**kwargs)
+		self.optimizer = optimizers.get(optimizer)
+		self.multipliers = multipliers
+		if hasattr(self.optimizer, 'learning_rate'):
+			self.lr_attr = 'learning_rate'
+		else:
+			self.lr_attr = 'lr'
+
+	@property
+	def lr(self):
+		return self.optimizer.lr
+
+	@lr.setter
+	def lr(self, lr):
+		self.optimizer.lr = lr
+
+	@property
+	def learning_rate(self):
+		return self.optimizer.learning_rate
+
+	@learning_rate.setter
+	def learning_rate(self, learning_rate):
+		try:
+			self.optimizer.learning_rate = learning_rate
+		except ValueError:
+			self.optimizer._hyper['learning_rate'] = learning_rate
+
+	def _get_multiplier(self, name):
+		multiplier, prefix_len = 1.0, 0
+		for key, val in self.multipliers.items():
+			if name.startswith(key):
+				if len(key) > prefix_len:
+					prefix_len = len(key)
+					multiplier = val
+		return multiplier
+
+	def get_updates(self, loss, params):
+		if len(self.updates) > 0:
+			return self.updates
+		multiplies = {}
+		for param in params:
+			multiplier = self._get_multiplier(param.name)
+			if multiplier not in multiplies:
+				multiplies[multiplier] = []
+			multiplies[multiplier].append(param)
+
+		self.updates, self.weights = [], []
+		origin_lr = getattr(self, self.lr_attr)
+		for i, (multiplier, params) in enumerate(multiplies.items()):
+			lr = origin_lr
+			if callable(multiplier):
+				lr = lr * multiplier(K.cast(self.optimizer.iterations, K.floatx()))
+			elif multiplier != 1.0:
+				lr = lr * multiplier
+			setattr(self, self.lr_attr, lr)
+			with K.name_scope('Group_{}'.format(i)):
+				self.updates += self.optimizer.get_updates(loss, params)
+			print(self.multipliers, i, self.optimizer.weights)
+			for w in K.cast(self.optimizer.weights, K.floatx()):
+				if w not in self.weights:
+					self.weights.append(w)
+		setattr(self, self.lr_attr, origin_lr)
+
+		return self.updates
+
+	def get_config(self):
+		config = {
+			'optimizer': optimizers.serialize(self.optimizer),
+			'multipliers': self.multipliers
+		}
+		base_config = super(LRMultiplierWrapper, self).get_config()
+		return dict(list(base_config.items()) + list(config.items()))
+
+	@classmethod
+	def from_config(cls, config):
+		optimizer = optimizers.deserialize(config.pop('optimizer'))
+		return cls(optimizer, **config)
