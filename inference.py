fe1751d6713965222470c6c5c554351cd999b8a9
Axel <avi.marsden@mail.huji.ac.il>
2020-09-07 21:21:47 +0300

    Added overlapping specs functionality

--- inference.py
+++ inference.py
@@ -104,15 +104,15 @@ class Inferer:
 
     def _concatenate_overlap(self, imgs):
         mask = binomial_mask()
-        first_in_pair = np.concatenate((imgs[0], np.zeros(128, 96)), axis=1)
-        second_in_pair = np.concatenate((np.zeros(128, 96), imgs[1]), axis=1)
+        first_in_pair = np.concatenate((imgs[0], np.zeros((128, 96))), axis=1)
+        second_in_pair = np.concatenate((np.zeros((128, 96)), imgs[1]), axis=1)
         merge = (1 - mask) * first_in_pair + mask * second_in_pair
         last_img = merge[:, -128:]
         full_spec = np.zeros((128, 1280))
         full_spec[:, 0: 2 * 128 - 32] = merge
         for i, img in zip(range(96, 1280 - 128, 96), imgs[1:]):
-            first_in_pair = np.concatenate((last_img, np.zeros(128, 96)), axis=1)
-            second_in_pair = np.concatenate((np.zeros(128, 96), img), axis=1)
+            first_in_pair = np.concatenate((last_img, np.zeros((128, 96))), axis=1)
+            second_in_pair = np.concatenate((np.zeros((128, 96)), img), axis=1)
             merge = (1 - mask) * first_in_pair + mask * second_in_pair
             last_img = merge[:, -128:]
             full_spec[:, i: i + 2 * 128 - 32] = merge
