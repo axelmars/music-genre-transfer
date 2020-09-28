9c782c60cea51fc1716cd44badb1e95a43a1344c
Axel <avi.marsden@mail.huji.ac.il>
2020-09-28 21:47:54 +0300

    Reverting to conv

--- model/network.py
+++ model/network.py
@@ -145,7 +145,7 @@ class Converter:
 		)
 		lr_scheduler = CosineLearningRateScheduler(max_lr=1e-4, min_lr=1e-5, total_epochs=n_epochs)
 		# lr_scheduler = CosineLearningRateScheduler(max_lr=1e-4, min_lr=1e-5, total_epochs=n_epochs)
-		early_stopping = EarlyStopping(monitor='loss', mode='min', min_delta=1, patience=100, verbose=1)
+		early_stopping = EarlyStopping(monitor='loss', mode='min', min_delta=0.001, patience=100, verbose=1)
 
 		tensorboard = EvaluationCallback(
 			imgs, identities,
@@ -198,7 +198,7 @@ class Converter:
 		)
 
 	def __l1_and_l2_loss(self, y_true, y_pred):
-		alpha = 0.5
+		alpha = 0
 		return (1-alpha) * tf.keras.losses.MeanAbsoluteError()(y_true, y_pred) + alpha * tf.keras.losses.MeanSquaredError()(y_true, y_pred)
 
 	def __perceptual_loss(self, y_true, y_pred):
