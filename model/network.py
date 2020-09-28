c3d44b78934069dcc971b554e974c51a64603d77
Axel <avi.marsden@mail.huji.ac.il>
2020-09-28 22:07:20 +0300

    Changed loss to .25 * MAE

--- model/network.py
+++ model/network.py
@@ -198,7 +198,7 @@ class Converter:
 		)
 
 	def __l1_and_l2_loss(self, y_true, y_pred):
-		alpha = 0.25
+		alpha = 0.8
 		return (1-alpha) * tf.keras.losses.MeanAbsoluteError()(y_true, y_pred) + alpha * tf.keras.losses.MeanSquaredError()(y_true, y_pred)
 
 	def __perceptual_loss(self, y_true, y_pred):
