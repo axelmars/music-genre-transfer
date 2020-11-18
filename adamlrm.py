813d8034f6e7ac67ee9525f6b150a421f99a4dd4
Axel <avi.marsden@mail.huji.ac.il>
2020-11-18 23:20:42 +0200

    Added clustering and vgg stuff

--- adamlrm.py
+++ adamlrm.py
@@ -318,6 +318,4 @@ class AdamLRM(optimizer_v2.OptimizerV2):
             if isinstance(config["learning_rate"], dict):
                 config["learning_rate"] = optimizer_v2.learning_rate_schedule.deserialize(
                     config["learning_rate"], custom_objects=custom_objects)
-        optimizer = cls(config)
-        optimizer._hypers_created = True
-        return optimizer
+        return cls(**config)
