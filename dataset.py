1fc0b307da60736f1c909eb1164c52103d504701
Axel <avi.marsden@mail.huji.ac.il>
2020-12-29 18:06:38 +0200

    Added clustering and vgg stuff

--- dataset.py
+++ dataset.py
@@ -393,8 +393,10 @@ class SimpleFMA(DataSet):
 
 			# img_name = re.search(r'\d+\.npy', spec_path).group(0)
 			img_name = spec_path
+
 			# img_path = os.path.join(self._base_dir, 'datasets', f'fma_medium_specs_overlap-{CLASS_1_ID}-{CLASS_2_ID}', img_name)
 			img_path = os.path.join(self._base_dir, 'datasets', 'solos_specs_train', img_name)
+			print(img_path)
 			img_paths.append(img_path)
 
 		return img_paths
